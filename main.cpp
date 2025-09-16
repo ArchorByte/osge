466889cced3a0fc2e331bedd03063bd3130f8de3
ArchorByte <archorbyte@gmail.com>
2025-09-16 20:03:40 +0200

    main.cpp cleanup

--- main.cpp
+++ main.cpp
@@ -23,28 +23,37 @@
 #include <map>
 #include <filesystem>
 
-// Handle the exceptions and trigger a crash only when we finished to clear memory and inform the player of the crash.
+// Note: If the graphics API provided is not handled, we use Vulkan by default.
+enum GraphicsAPI
+{
+    VULKAN = 0,
+    OPENGL = 1
+};
+
+// Handle the exceptions.
 void crash_handler
 (
     const std::string &error
 )
 {
-    error_log("Note: Every object created by the engine has been destroyed before crashing to prevent any memory overload or memory leak.");
-    open_crash_popup(error); // Open a message box to inform the player of the game crash.
+    error_log("The program crashed! Every object created by the engine has been destroyed to prevent any memory overload or memory leak.");
+    open_crash_popup(error); // Open a message box to inform of the project crash.
     fatal_error_log(error);  // Finally trigger the crash.
 }
 
+// Code executed on start.
 int main()
 {
     try
     {
         #ifdef ENABLE_LOGS_FILE
-            if (std::filesystem::exists(LOGS_FILE_NAME)) std::filesystem::remove(LOGS_FILE_NAME); // We reset the logs file by deleting it if it already exists.
+            // We "reset" the logs file by deleting it if it already exists.
+            if (std::filesystem::exists(LOGS_FILE_NAME)) std::filesystem::remove(LOGS_FILE_NAME);
             create_new_empty_file(LOGS_FILE_NAME);
         #endif
 
-        // We disable the console on Windows if we're in release mode.
         #ifndef DEBUG_MODE
+            // We get rid of the console on Windows if we are running in release mode.
             #if defined(_WIN64)
                 hide_console();
             #endif
@@ -52,76 +61,77 @@ int main()
             log("WARNING: Running in debug mode!");
         #endif
 
-        log("Running on OSGE v" + std::to_string(ENGINE_VERSION_VARIANT) + "." + std::to_string(ENGINE_VERSION_MAJOR) + "." + std::to_string(ENGINE_VERSION_MINOR) + "." + std::to_string(ENGINE_VERSION_PATCH) + ".");
-        start_sdl3_instance();
-        check_operating_system_support();                              // Check if we are running on a supported operating system.
-        std::vector<int> display_indexes = get_available_displays(); // Retrieve all available monitors on this device.
+        std::string engine_version_variant = std::to_string(ENGINE_VERSION_VARIANT);
+        std::string engine_version_major = std::to_string(ENGINE_VERSION_MAJOR);
+        std::string engine_version_minor = std::to_string(ENGINE_VERSION_MINOR);
+        std::string engine_version_patch = std::to_string(ENGINE_VERSION_PATCH);
 
-        // We create a new default game config file if its missing.
+        log("Running on OSGE v" + engine_version_variant + "." + engine_version_major + "." + engine_version_minor + "." + engine_version_patch + "!");
+        start_sdl3_instance();
+
+        check_operating_system_support();                            // Check if we are running on a supported operating system.
+        std::vector<int> display_indexes = get_available_displays(); // Retrieve and list the index of all available monitors on this device.
+
+        // We create a new default game config file if it's missing.
         if (!std::filesystem::exists("game.config"))
         {
             create_new_empty_file("game.config");
             write_file("game.config", "WINDOW_WIDTH=1920\nWINDOW_HEIGHT=1080\nWINDOW_MODE=2\nGRAPHICS_API=0\nMONITOR=1\nGPU=1", false, true);
         }
 
-        // Read the data in the config file and parse the data into a map.
+        // Read the data in the game.config file and parse its data into a map.
         std::map<std::string, std::string> config = parse_config_file("game.config");
 
-        // Read the config file content.
-        std::string window_width = config["WINDOW_WIDTH"];
-        std::string window_height = config["WINDOW_HEIGHT"];
+        // Retrieve the data from the game.config file.
         std::string window_mode = config["WINDOW_MODE"];
         std::string graphics_api = config["GRAPHICS_API"];
         std::string monitor = config["MONITOR"];
-
-        // Note: If the graphics API provided is not handled, we use Vulkan by default.
-        enum GraphicsAPI
-        {
-            VULKAN = 0,
-            OPENGL = 1
-        };
+        std::string gpu = config["GPU"];
 
         if (!is_an_integer(window_mode))
         {
-            error_log("Invalid window mode detected! Switched to full screen by default!");
+            error_log("Invalid window mode configured! Switched to full screen by default!");
             window_mode = "2";
         }
 
         if (!is_an_integer(graphics_api))
         {
-            error_log("Invalid graphics API detected! Switched to Vulkan by default!");
+            error_log("Invalid graphics API configured! Switched to Vulkan by default!");
             graphics_api = "0";
         }
 
         if (!is_an_integer(monitor))
         {
-            error_log("Invalid monitor detected! Switched to the primary monitor by default!");
+            error_log("Invalid monitor configured! Switched to the primary monitor by default!");
             monitor = "1";
         }
 
-        int int_monitor = stoi(monitor);
+        int monitor_index = stoi(monitor);
 
-        if (int_monitor < 1 || int_monitor > display_indexes.size())
+        if (monitor_index < 1 || monitor_index > display_indexes.size())
         {
             error_log("Invalid monitor detected! Switched to the primary monitor by default!");
-            int_monitor = 1;
+            monitor_index = 1;
         }
 
-        // Retrieve the screen resolution.
-        std::pair<int, int> screen_resolution = get_display_resolution(display_indexes[int_monitor - 1]);
+        // Get the screen resolution.
+        // Screen resolution store as <width, height>.
+        std::pair<int, int> screen_resolution = get_display_resolution(display_indexes[monitor_index - 1]);
 
         // List the "allowed" game resolutions.
-        // Those resolutions are the 16/9 resolutions that can be used on this screen.
+        // TODO: Add more screen ratio to the allowed game resolutions.
         std::vector<std::string> allowed_game_resolutions = get_allowed_game_resolutions(screen_resolution);
 
         // Select the window resolution at start.
-        // We try to use the resolution in the config file.
-        // If it's not a 16/9 resolution, we try to use the screen resolution.
-        // If it's still not a 16/9 resolution, we select the highest allowed resolution.
+        // Selection priorities: game.config res, screen res and finally highest allowed res.
         std::pair<int, int> game_resolution = select_game_resolution(config, screen_resolution, allowed_game_resolutions);
 
-        // We create the main SDL3 window.
-        SDL3_Window window(game_resolution.first, game_resolution.second, stoi(window_mode), std::string(GAME_TITLE), stoi(graphics_api));
+        int window_width = game_resolution.first;
+        int window_height = game_resolution.second;
+        std::string window_title = std::string(GAME_TITLE);
+
+        // We create the SDL3 window of the game.
+        SDL3_Window window(window_width, window_height, stoi(window_mode), window_title, stoi(graphics_api));
 
         // Select the graphics API that we are going to use.
         switch (stoi(graphics_api))
@@ -134,7 +144,7 @@ int main()
                 run_opengl();
                 break;
 
-            // If the graphics API provided by the config file is not valid, we use Vulkan by default.
+            // If the graphics API provided by the game.config file is not handled, we default to Vulkan.
             default:
                 run_vulkan(window.get());
                 break;
@@ -142,23 +152,23 @@ int main()
 
         return 0;
     }
-    catch (const std::exception &error) // Catch exceptions errors.
+    catch (const std::exception &error)
     {
         crash_handler(error.what());
     }
-    catch (const std::string &error)    // Catch string errors.
+    catch (const std::string &error)
     {
         crash_handler(error);
     }
-    catch (const char &error)           // Catch char errors.
+    catch (const char &error)
     {
         crash_handler(std::to_string(error));
     }
-    catch (const char* &error)          // Catch char* errors.
+    catch (const char* &error)
     {
         crash_handler(std::string(error));
     }
-    catch (const int &error)            // Catch integer errors.
+    catch (const int &error)
     {
         crash_handler(std::to_string(error));
     }
