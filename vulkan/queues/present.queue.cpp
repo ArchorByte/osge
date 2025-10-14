994333e1c77e47b8b3cefddc28f24847ea11746f
ArchorByte <archorbyte@gmail.com>
2025-10-14 19:59:25 +0200

    Refactor 11

--- vulkan/queues/present.queue.cpp
+++ vulkan/queues/present.queue.cpp
@@ -22,12 +22,12 @@ uint32_t get_present_family_index
         fatal_error_log("Present family index query failed! No queue families were provided!");
     }
 
-    if (!physical_device || physical_device == VK_NULL_HANDLE)
+    if (physical_device == VK_NULL_HANDLE)
     {
         fatal_error_log("Present family index query failed! The physical device provided (" + force_string(physical_device) + ") is not valid!");
     }
 
-    if (!vulkan_surface || vulkan_surface == VK_NULL_HANDLE)
+    if (vulkan_surface == VK_NULL_HANDLE)
     {
         fatal_error_log("Present family index query failed! The Vulkan surface provided (" + force_string(vulkan_surface) + ") is not valid!");
     }
@@ -35,19 +35,18 @@ uint32_t get_present_family_index
     uint32_t output = -1;
     int i = 0;
 
-    // Analyze each queue family registered.
     for (const VkQueueFamilyProperties &queue : queue_families)
     {
-        // Try to determine if the physical device handles the surfaces.
-        VkBool32 support = VK_FALSE;
-        VkResult query_result = vkGetPhysicalDeviceSurfaceSupportKHR(physical_device, i, vulkan_surface, &support);
+        // Try to determine if the physical device handles the Vulkan surface.
+        VkBool32 supported = VK_FALSE;
+        const VkResult query_result = vkGetPhysicalDeviceSurfaceSupportKHR(physical_device, i, vulkan_surface, &supported);
 
         if (query_result != VK_SUCCESS)
         {
             fatal_error_log("Physical device surface support query returned error code " + std::to_string(query_result) + ".");
         }
 
-        if (support)
+        if (supported)
         {
             output = i;
             break;
